ed315fe6a28a0d2368c10e8909e7a1b66d4ad983
unknown <3283241528@qq.com>
2024-08-01 12:05:09 +0800

    Site updated: 2024-08-01 12:05:09

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722484961699";
+const VERSION = "1722485074899";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
