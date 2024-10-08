1709ad97df82986f2a1c01ea8ce00149cd229314
lim <3283245128@qq.com>
2024-10-09 07:50:45 +0800

    Site updated: 2024-10-09 07:50:45

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1725874930450";
+const VERSION = "1728431437978";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
