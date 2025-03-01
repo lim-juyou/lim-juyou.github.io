153f2167a41912000b8232c45ab4f7ee42650e04
joyou <14174883+joyou11@user.noreply.gitee.com>
2025-03-02 00:05:47 +0800

    Site updated: 2025-03-02 00:05:46

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1740574938847";
+const VERSION = "1740845140585";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
