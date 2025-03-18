e207f7e12e132e2c59d61c75652808f551566e73
joyou <14174883+joyou11@user.noreply.gitee.com>
2025-03-18 20:54:37 +0800

    Site updated: 2025-03-18 20:54:36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1742302099648";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1742302465002";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
